8cdedc97908c55299f4dbd91cfef180f3cb37e7d
John Clements <clements@racket-lang.org>
2020-05-13 15:13:47 -0700

    add files

---
+++ info.rkt
@@ -0,0 +1,14 @@
+#lang setup/infotab
+
+(define collection 'multi)
+(define version "0.1") ; version bump for add of s16vec-record
+
+#;(define deps
+  (list "base"))
+
+#;(define build-deps
+  (list "rackunit-lib"
+        "scribble-lib"
+         "racket-doc"))
+
+
