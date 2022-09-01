524821758aefa6aa6af12d08c35ff520ae4ff543
John Clements <clements@racket-lang.org>
2022-09-01 19:57:29 -0400

    use new foreign lib setup

--- info.rkt
+++ info.rkt
@@ -1,14 +1,9 @@
 #lang setup/infotab
 
-(define collection 'multi)
-(define version "0.1") ; version bump for add of s16vec-record
-
-#;(define deps
-  (list "base"))
-
-#;(define build-deps
-  (list "rackunit-lib"
-        "scribble-lib"
-         "racket-doc"))
+(define version "0.1")
+(define deps '("base"))
+(define copy-foreign-libs '("libportaudio.2.dylib"
+                            "librsoundcallbacks.dylib"))
+(define install-platform #rx"x86_64-macosx")
 
 
